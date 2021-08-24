0331ea85fbc218730be803fed4be408e27374085
Crowdbotics <team@crowdbotics.com>
2021-08-24 02:41:13 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "weathered_bonus_30066Identifier",
+  url: "https://weathered_bonus_30066Identifier.botics.co",
+  api: "https://weathered_bonus_30066Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
